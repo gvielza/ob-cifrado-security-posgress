f80c778379be179a910ec3106a0db06f6c51cdcb
gvielza <80008338+gvielza@users.noreply.github.com>
2022-07-31 20:37:09 -0300

    cifrado con s security, postgress

---
+++ src/main/java/obsecuritycifrado/ObSecurityCifradoApplication.java
@@ -0,0 +1,13 @@
+package obsecuritycifrado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObSecurityCifradoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObSecurityCifradoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/obsecuritycifrado/ObSecurityCifradoApplicationTests.java
@@ -0,0 +1,13 @@
+package obsecuritycifrado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObSecurityCifradoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
